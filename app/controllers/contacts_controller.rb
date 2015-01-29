1d8ee51f61fd3b58b310528da02618119dc4f82b
Marvin Santizo <santizomarvin88@gmail.com>
2015-01-28 21:08:06 -0700

    installed simpleform with bootstrap also added method actions for contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,20 @@
 class ContactsController < ApplicationController
   def new
+  	@contact = Contact.new
   end
 
   def create
+  	@contact = Contact.new(contact_params)
+  	if @contact.save
+  		 redirect_to root_path, :notice ("Message sent")
+  	else
+  		render :new
+  	end
+  end
+
+  private
+
+  def contact_params
+  	params.require(:contact).permit(:name, :email, :message)
   end
 end
